5a77fb9b92aa4c76df34fcc5a81f7e3cb4b5c2c8
Velithris <13710677+Zireael-N@users.noreply.github.com>
2024-03-23 16:00:00 +0000

    Store names of the modules in YAML files

--- localize_npc_names/src/main.rs
+++ localize_npc_names/src/main.rs
@@ -1,8 +1,9 @@
 use indexmap::IndexMap as Map;
+use serde::Deserialize;
 use std::{
     env,
     fs::{self, File},
-    io::BufReader,
+    io::{BufReader, Seek, SeekFrom},
     path::PathBuf,
 };
 
@@ -11,6 +12,12 @@ use localize_npc_names::{Error, Localizer};
 #[cfg(all(target_env = "musl", target_pointer_width = "64"))]
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[derive(Debug, Deserialize)]
+struct InputFile {
+    module_name: Option<String>,
+    npcs: Map<String, i64>,
+}
 
 fn main() -> Result<(), Error> {
     let (yaml_path, output_dir, module_name) = {
@@ -38,12 +45,37 @@ fn main() -> Result<(), Error> {
         }
     };
 
-    let ids_map: Map<String, i64> =
-        serde_yaml::from_reader(BufReader::new(File::open(&yaml_path)?))?;
     let force_all = matches!(env::var_os("FORCE_ALL"), Some(ref v) if v == "1");
-    let module_name = match module_name {
-        Some(module_name) => module_name,
-        None => format!("{} Trash", yaml_path.file_stem().unwrap().to_string_lossy()),
+    let mut input_file = BufReader::new(File::open(&yaml_path)?);
+    let (ids_map, module_name) = match serde_yaml::from_reader::<_, InputFile>(&mut input_file) {
+        Ok(input) => {
+            let module_name = match (module_name, input.module_name) {
+                (Some(inner), _) => inner,
+                (_, Some(inner)) => inner,
+                _ => {
+                    eprintln!(
+                        "WARNING: module_name is missing, falling back to using the file's name"
+                    );
+                    format!(
+                        "{} Trash",
+                        &yaml_path.file_stem().unwrap().to_string_lossy()
+                    )
+                }
+            };
+            (input.npcs, module_name)
+        }
+        Err(_) => {
+            input_file.seek(SeekFrom::Start(0))?;
+            let ids_map = serde_yaml::from_reader(&mut input_file)?;
+            let module_name = match module_name {
+                Some(module_name) => module_name,
+                None => format!(
+                    "{} Trash",
+                    &yaml_path.file_stem().unwrap().to_string_lossy()
+                ),
+            };
+            (ids_map, module_name)
+        }
     };
 
     fs::create_dir_all(&output_dir)?;
